ddb4fd37d03daa6405e401c6795bfdb697788a67
enio.moura <enio.moura@goias.gov.br>
2020-11-09 18:30:07 -0300

    adicionando links inciso 3

--- src/protected/application/themes/Goias/layouts/parts/aldirblanc.php
+++ src/protected/application/themes/Goias/layouts/parts/aldirblanc.php
@@ -1,3 +1,10 @@
 <div class='box'>
+    <p>Faça sua inscrição nos Editais do Inciso III - Lei Aldir Blanc. Secult - GO:
+        <br>
+        <a href="https://mapagoiano.cultura.go.gov.br/oportunidade/92/" style="color: #66cc66;">Edital I</a>
+        <br>
+        <a href="https://mapagoiano.cultura.go.gov.br/oportunidade/91/" style="color: #66cc66;">Edital II</a>
+        <br>
+        <a href="https://mapagoiano.cultura.go.gov.br/oportunidade/90/" style="color: #66cc66;">Edital III</a>
     <p><a style='color: white' href='https://drive.google.com/file/d/1qDrpfH3Rl7dQg3cW3G6lMNopIjDq7ZS6/view'>Clique aqui e saiba como aplicar a Lei de Emergência Cultural no seu município!</a></p>
 </div>
