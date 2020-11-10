fd9af4b79e3ae16c0c8b6a159bb4fc125916e407
enio.moura <enio.moura@goias.gov.br>
2020-11-10 11:12:36 -0300

    alteracao botoes home inciso 3

--- src/protected/application/themes/Goias/layouts/parts/aldirblanc.php
+++ src/protected/application/themes/Goias/layouts/parts/aldirblanc.php
@@ -1,10 +1,11 @@
-<div class='box'>
+<div class="box">
     <p>Faça sua inscrição nos Editais do Inciso III - Lei Aldir Blanc. Secult - GO:
+        <br> <br>
+        <a class="btn btn-accent btn-large" href="https://mapagoiano.cultura.go.gov.br/oportunidade/92/" style="color: #FFF;">Edital I</a>  
         <br>
-        <a href="https://mapagoiano.cultura.go.gov.br/oportunidade/92/" style="color: #66cc66;">Edital I</a>
+        <a class="btn btn-accent btn-large" href="https://mapagoiano.cultura.go.gov.br/oportunidade/91/" style="color: #FFF;">Edital II</a>  
         <br>
-        <a href="https://mapagoiano.cultura.go.gov.br/oportunidade/91/" style="color: #66cc66;">Edital II</a>
-        <br>
-        <a href="https://mapagoiano.cultura.go.gov.br/oportunidade/90/" style="color: #66cc66;">Edital III</a>
-    <p><a style='color: white' href='https://drive.google.com/file/d/1qDrpfH3Rl7dQg3cW3G6lMNopIjDq7ZS6/view'>Clique aqui e saiba como aplicar a Lei de Emergência Cultural no seu município!</a></p>
+        <a class="btn btn-accent btn-large" href="https://mapagoiano.cultura.go.gov.br/oportunidade/90/" style="color: #FFF;">Edital III</a>  
+    </p>
+    <p><a style="color: white" href="https://drive.google.com/file/d/1qDrpfH3Rl7dQg3cW3G6lMNopIjDq7ZS6/view">Clique aqui e saiba como aplicar a Lei de Emergência Cultural no seu município!</a></p>
 </div>
