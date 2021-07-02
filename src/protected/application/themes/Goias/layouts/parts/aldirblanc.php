e7d50d99b07f553d3b3e43e0b9eafa68b5c12b00
enio.moura <enio.moura@goias.gov.br>
2021-07-02 19:54:49 -0300

    removendo texto FAC

--- src/protected/application/themes/Goias/layouts/parts/aldirblanc.php
+++ src/protected/application/themes/Goias/layouts/parts/aldirblanc.php
@@ -1,8 +1,8 @@
-<div class="box">
+<!-- <div class="box">
     <h1>Fundo de Apoio a Cultura:</h1>
     <p></p>
     <p>Faça sua inscrição nos Editais do Fundo de Apoio a Cultura:
         <br> <br>
         <a class="btn btn-accent btn-large" style="color: #FFF;" href="https://mapagoiano.cultura.go.gov.br/oportunidade/153">CLIQUE AQUI</a>  
     </p>
-</div>
+</div> -->
