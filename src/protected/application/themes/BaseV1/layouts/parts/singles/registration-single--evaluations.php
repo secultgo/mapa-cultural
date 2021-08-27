6457dbc88461fb0e50def214378fb44cd1b825a6
Rodrigo Marconato <rodrigo.marconato@prognus.com.br>
2021-08-27 15:07:47 -0300

    Remover botão para solicitar recurso no formulário de inscrição e incluir link para página da oportunidade

--- src/protected/application/themes/BaseV1/layouts/parts/singles/registration-single--evaluations.php
+++ src/protected/application/themes/BaseV1/layouts/parts/singles/registration-single--evaluations.php
@@ -1,13 +1,13 @@
 <div ng-if="<?php echo sizeof($entity->getUserEvaluations()) > 0; ?>" id="registration-evaluations" class="registration-fieldset">
     
     <h4><?php \MapasCulturais\i::_e("Avaliações");?></h4>
-
-    <?php
-        $reg_args = ['registration' => $entity, 'opportunity' => $opportunity]; 
-    ?>
     
-    <?php $this->applyTemplateHook('registration-evaluations-field-list', 'before', $reg_args) ?>
+    <?php $this->applyTemplateHook('registration-evaluations-field-list', 'before') ?>
     <?php echo $entity->getUserEvaluationsString(); ?>
-    <?php $this->applyTemplateHook('registration-evaluations-field-list', 'after', $reg_args) ?>
-    
+    <?php $this->applyTemplateHook('registration-evaluations-field-list', 'after') ?>
+
+    <?php if($entity->canUser('sendClaimMessage')): ?>
+        <a href="<?php echo $opportunity->singleUrl ?>"><?php echo \MapasCulturais\i::_e("Ir para página da oportunidade para solicitar o recurso"); ?></a>
+    <?php endif ?>
+
 </div>
