8972741cb91bc927a09065418cca71ed0b02972d
Victor Ferreira de Souza <victor.souza@goias.gov.br>
2022-02-22 17:24:45 +0000

    Revert "Update header-logo.php - Google Analytics"
    This reverts commit 790e827965da9c87372e677c2f14ca6fa7454e7f

--- src/protected/application/themes/Goias/layouts/parts/header-logo.php
+++ src/protected/application/themes/Goias/layouts/parts/header-logo.php
@@ -1,11 +1,1 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-156304994-2"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-156304994-2');
-</script>
-
 <h1 id="brand-logo"><a href="<?php echo $app->getBaseUrl() ?>"><img src="<?php $this->asset('img/logo-site.png'); ?>" /></a></h1>
